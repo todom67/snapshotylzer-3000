1165eca105032920e5d907242e1e5cd4c740d985
Timothy Odom <tim.odom@thinkahead.com>
2018-11-11 23:16:09 -0600

    add the start of cli goodness

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,7 +1,22 @@
 import boto3
+import click
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+    
+    return  
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
+    
